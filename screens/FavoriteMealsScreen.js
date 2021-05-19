5b33ec6c595d3025e55f717ea8386b9e88406ebf
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2021-05-19 21:18:52 +0100

    added MealList component to render list of MealItems

--- screens/FavoriteMealsScreen.js
+++ screens/FavoriteMealsScreen.js
@@ -5,16 +5,12 @@ import MealList from "../components/MealList";
 import { MEALS } from "../data/dummy-data";
 
 export default function FavoriteMealsScreen(props) {
-	const favoriteMeals = [];
-	for (meal of MEALS) {
-		if (meal.id === "m1" || meal.id === "m2") {
-			favoriteMeals.push(meal);
-		}
-	}
+	const favoriteMeals = MEALS.filter(
+		meal => meal.id === "m1" || meal.id === "m2"
+	);
 	return <MealList listData={favoriteMeals} navigation={props.navigation} />;
 }
 
 FavoriteMealsScreen.navigationOptions = {
 	headerTitle: "Your Favorite Meals...",
 };
-
